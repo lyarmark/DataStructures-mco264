e03352705c164ecd5f2d9858e2a9a060a3394b42
Leah Yarmark <lyarmark@student.touro.edu>
2015-11-04 00:53:48 -0500

    final decimalToBinary

--- MCO 264 Data Structures/src/decimalToBinary/DecimalToBinary.java
+++ MCO 264 Data Structures/src/decimalToBinary/DecimalToBinary.java
@@ -4,26 +4,27 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class DecimalToBinary {
+	private Stack<Integer> stack = new Stack<Integer>();
+	
+	public String decimalToBinary(int decimal) {	
 
-	public static String decimalToBinary(int decimal) {
-		Stack <Integer> stack = new Stack<Integer>();
-		
 		do {
 			stack.push(decimal % 2);
-		decimal = (decimal - (decimal % 2)) / 2;	
-		}  while (decimal > 0);
-		
+			decimal = decimal / 2;
+		} while (decimal > 0);
+
 		StringBuilder binary = new StringBuilder();
 		while (!stack.isEmpty()) {
 			binary.append(stack.pop());
 		}
 		return binary.toString();
 	}
-	
+
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
 		System.out.println("Enter a decimal number to be converted to binary.");
 		int number = keyboard.nextInt();
-		System.out.println(decimalToBinary(number));
+		DecimalToBinary dtb = new DecimalToBinary();
+		System.out.println(dtb.decimalToBinary(number));
 	}
 }
