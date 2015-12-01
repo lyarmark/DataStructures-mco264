e2c6d97d6ed83fd0a905364a33025983f67666da
Leah Yarmark <lyarmark@student.touro.edu>
2015-12-01 00:56:39 -0500

    working non recursive blobs

--- MCO 264 Data Structures/src/blobsRecursive/GridNonRecursive.java
+++ MCO 264 Data Structures/src/blobsRecursive/GridNonRecursive.java
@@ -10,8 +10,8 @@ public class GridNonRecursive<E> {
 	private Stack<Cell<E>> stack;
 
 	public GridNonRecursive(int rows, int columns) {
-		stack = new Stack<Cell<E>>();
 		cells = new Vector<Vector<Cell<E>>>();
+		stack = new Stack<Cell<E>>();
 
 		// initialize each cell of the grid
 		for (int row = 0; row < rows; row++) {
@@ -54,7 +54,8 @@ public class GridNonRecursive<E> {
 		for (int row = 0; row < cells.size(); row++) {
 			for (int col = 0; col < cells.get(row).size(); col++) {
 				Cell<E> startCell = cells.get(row).get(col);
-				System.out.println("starting at cell [" + row + "][" + col + "]");
+				// System.out.println("starting at cell [" + row + "][" + col +
+				// "]");
 				if (!startCell.isVisited() && startCell.hasData()) {
 					count++;
 					// mark the blob connected to starting cell
@@ -66,89 +67,44 @@ public class GridNonRecursive<E> {
 	}
 
 	public void markBlob(Cell<E> currentCell) {
-		// mark the current cell as visited
-		System.out.println("current cell " + currentCell.getRow() + " " + currentCell.getCol());
-
-		currentCell.setVisited();
 		stack.push(currentCell);
-		if (checkTop(currentCell)) {
+		do {
 
-		}
+			currentCell.setVisited();
 
-		while (!stack.isEmpty()) {
-			while (!checkTop(currentCell)) {
-				stack.push(currentCell);
+			// traverse up
+			if (currentCell.getRow() > 0) { // end of grid
+				Cell<E> newCell = cells.get(currentCell.getRow() - 1).get(currentCell.getCol());
+				if ((!newCell.isVisited() && newCell.hasData())) {
+					stack.push(newCell);
+				}
 			}
 
-			currentCell = stack.peek();
-
-			while (!checkRight(currentCell)) {
-				stack.push(currentCell);
+			// traverse down
+			if (currentCell.getRow() < cells.size() - 1) { // move down one row
+				Cell<E> newCell = cells.get(currentCell.getRow() + 1).get(currentCell.getCol());
+				if ((!newCell.isVisited() && newCell.hasData())) {
+					stack.push(newCell);
+				}
 			}
 
-			currentCell = stack.peek();
-
-			while (!checkLeft(currentCell)) {
-				stack.push(currentCell);
+			// traverse left
+			if (currentCell.getCol() > 0) { // end of grid
+				Cell<E> newCell = cells.get(currentCell.getRow()).get(currentCell.getCol() - 1);
+				if ((!newCell.isVisited() && newCell.hasData())) {
+					stack.push(newCell);
+				}
 			}
 
-			currentCell = stack.peek();
-
-			while (!checkBottom(currentCell)) {
-				stack.push(currentCell);
+			// traverse right
+			if (currentCell.getCol() < cells.get(currentCell.getRow()).size() - 1) {
+				Cell<E> newCell = cells.get(currentCell.getRow()).get(currentCell.getCol() + 1);
+				if (!newCell.isVisited() && newCell.hasData()) {
+					stack.push(newCell);
+				}
 			}
-
-		}
-		/*
-		 * // traverse up if (currentCell.getRow() > 0) { // end of grid // move
-		 * up one row and mark blob starting there
-		 * System.out.println("moved up from cell " + currentCell + " " +
-		 * currentCell.getRow() + " " + currentCell.getCol());
-		 * markBlob(cells.get(currentCell.getRow() -
-		 * 1).get(currentCell.getCol())); }
-		 * 
-		 * // traverse down if (currentCell.getRow() < cells.size() - 1) { //
-		 * move down one row System.out.println("moved down from cell " +
-		 * currentCell + " " + currentCell.getRow() + " " +
-		 * currentCell.getCol()); markBlob(cells.get(currentCell.getRow() +
-		 * 1).get(currentCell.getCol())); } // traverse left if
-		 * (currentCell.getCol() > 0) { // end of grid // move over left one
-		 * column System.out.println("moved left from cell " + currentCell + " "
-		 * + currentCell.getRow() + " " + currentCell.getCol());
-		 * markBlob(cells.get(currentCell.getRow()).get(currentCell.getCol() -
-		 * 1)); }
-		 * 
-		 * // traverse right
-		 * 
-		 * if (currentCell.getCol() < cells.get(currentCell.getRow()).size() -
-		 * 1) {
-		 * 
-		 * // move over right one column
-		 * System.out.println("moved right from cell " + currentCell + " " +
-		 * currentCell.getRow() + " " + currentCell.getCol());
-		 * markBlob(cells.get(currentCell.getRow()).get(currentCell.getCol() +
-		 * 1)); }
-		 */
-	}
-
-	public boolean checkTop(Cell<E> currentCell) {
-		Cell<E> next = new Cell<E>(currentCell.getRow() - 1, currentCell.getCol());
-		return next.hasData();
-	}
-
-	public boolean checkRight(Cell<E> currentCell) {
-		Cell<E> next = new Cell<E>(currentCell.getRow(), currentCell.getCol() + 1);
-		return next.hasData();
-	}
-
-	public boolean checkLeft(Cell<E> currentCell) {
-		Cell<E> next = new Cell<E>(currentCell.getRow(), currentCell.getCol() - 1);
-		return next.hasData();
-	}
-
-	public boolean checkBottom(Cell<E> currentCell) {
-		Cell<E> next = new Cell<E>(currentCell.getRow() + 1, currentCell.getCol());
-		return next.hasData();
+			currentCell = stack.pop();
+		} while (!stack.isEmpty());
 	}
 
 	public String toString() {
@@ -165,7 +121,7 @@ public class GridNonRecursive<E> {
 
 	static public void main(String[] args) {
 		Character character = new Character('X');
-		GridRecursive<Character> theGrid = new GridRecursive<Character>(5, 5);
+		GridNonRecursive<Character> theGrid = new GridNonRecursive<Character>(5, 5);
 		theGrid.setGrid(40, 'X');
 		System.out.println(theGrid.toString());
 		System.out.println(theGrid.countBlobs(character));
--- MCO 264 Data Structures/src/blobsRecursive/UserInterface.java
+++ MCO 264 Data Structures/src/blobsRecursive/UserInterface.java
@@ -3,15 +3,15 @@ package blobsRecursive;
 public class UserInterface {
 
 	public static void main(String[] args) {
-		
-		Character character = new Character('X');
-		
-		//can't figure out how define a grid of cell objects
-		//each cell can store an object (among other things)
-		GridRecursive<Character> theGrid = new GridRecursive<Character>(10,10);
-		theGrid.setGrid(30, 'X');
+
+		Cell<Character> cell = new Cell<Character>('X', 0, 0);
+
+		// can't figure out how define a grid of cell objects
+		// each cell can store an object (among other things)
+		GridNonRecursive<Cell<Character>> theGrid = new GridNonRecursive<Cell<Character>>(10, 10);
+		theGrid.setGrid(40, cell);
 		System.out.println(theGrid.toString());
-		System.out.println(theGrid.countBlobs(character));
-	
+		System.out.println(theGrid.countBlobs(cell));
+
 	}
 }
